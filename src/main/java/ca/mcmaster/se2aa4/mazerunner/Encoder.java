964fa40b2e115835b117e91667f19c6e8f6c5bf1
tridibbanik17 <tridib.perfect@gmail.com>
2025-01-23 14:09:34 -0500

    Add a function to encode a the solution path (close #3)|

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Encoder.java
@@ -0,0 +1,29 @@
+
+// import java.util.Scanner;
+
+public class Encoder {
+    public static String encoder(String longText) {
+        if (longText.length() == 0) {
+            throw new IllegalArgumentException("There is no path available.");
+        }
+
+        StringBuilder encodedText = new StringBuilder("");
+        for (int i = 0; i < longText.length(); i++) {
+            int count = 1;
+            while ((i < longText.length() - 1) && (longText.charAt(i) == longText.charAt(i+1))) {
+                count++;
+                i++;
+            }
+            encodedText.append(String.valueOf(longText.charAt(i)) + count);
+        }
+        return encodedText.toString();
+    }
+
+
+    // public static void main(String[] args) {
+    //     Scanner scanner = new Scanner(System.in);
+    //     System.out.println("Enter");
+    //     String str = scanner.nextLine();
+    //     System.out.println("Encoded string is " + encoder(str));
+    // }
+}
