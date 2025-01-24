c4c637c94c30a818ff8cdacfd0773a9bc8063b90
tridibbanik17 <tridib.perfect@gmail.com>
2025-01-23 22:32:41 -0500

    Add initial version of main

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -2,6 +2,8 @@ package ca.mcmaster.se2aa4.mazerunner;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -13,7 +15,17 @@ import org.apache.logging.log4j.Logger;
 
 public class Main {
 
+    // Custom exception to be thrown when a path is not found
+    public class PathNotFoundException extends Exception {
+
+        // Constructor to allow custom error message
+        public PathNotFoundException(String message) {
+            super(message);  // Pass the message to the superclass constructor
+        }
+    }
+
     private static final Logger logger = LogManager.getLogger();
+
     public static void main(String[] args) {
         logger.info("** Starting Maze Runner");
 
@@ -39,27 +51,90 @@ public class Main {
 
             // Read and process the maze file
             BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+            // logger.info("The text file read is", reader);
+            List<String> lines = new ArrayList<>();
             String line;
+
             while ((line = reader.readLine()) != null) {
-                for (int idx = 0; idx < line.length(); idx++) {
-                    if (line.charAt(idx) == '#') {
-                        logger.trace("WALL ");
-                    } else if (line.charAt(idx) == ' ') {
-                        logger.trace("PASS ");
-                    }
-                }
-                logger.trace("");
+                logger.info("Each line is {} ", line);
+                lines.add(line);
             }
-            
+            logger.info("Lines list {} ", lines);
+
             reader.close();
 
-        } catch(ParseException e) {
+
+            // Find the maximum length of the lines
+            int maxLength = 0;
+            for (String item : lines) {
+                if (item.length() > maxLength) {
+                    maxLength = item.length();
+                }
+            }
+
+            Character[][] mazeArray = new Character[lines.size()][maxLength];
+
+            // Fill the 2D array with characters from the lines
+            for (int i = 0; i < lines.size(); i++) {
+                String str = lines.get(i);
+                for (int j = 0; j < maxLength; j++) {
+                    if (j < str.length()) {
+                        mazeArray[i][j] = str.charAt(j);
+                    } else {
+                        mazeArray[i][j] = ' ';  // Fill with empty space if the line is shorter
+                    }
+                }
+            }
+
+            logger.info("Maze read with {} rows and {} columns.", lines.size(), lines.get(0).length());
+            logger.debug("Maze Array: {}", (Object) mazeArray);
+
+            logger.info("lines : {} ", lines);
+            logger.info("The size of the maze: {}", lines.size());
+            logger.info("mazeArray: {}", (Object) mazeArray);
+            // Create the Maze object
+            Maze maze = new Maze(mazeArray);
+            logger.info("Maze object created: {}", maze);
+            int entryRow = maze.getEntryRow();
+            int entryCol = maze.getEntryCol();
+            int exitRow = maze.getExitRow();
+            int exitCol = maze.getExitCol();
+
+
+            logger.info("entryRow: {}", entryRow);
+            logger.info("entryCol: {}", entryCol);
+            logger.info("exitRow: {}" , exitRow);
+            logger.info("exitCol: {}", exitCol);
+
+            // Log entry and exit points
+            logger.info("Entry point: (" + entryRow + ", " + entryCol + ")");
+            logger.info("Exit point: (" + exitRow + ", " + exitCol + ")");
+
+            // Initialize the MazeSolver
+            MazeSolver solver = new MazeSolver(maze);
+            logger.info("solver : " + solver);
+            
+
+            // Solve the maze using the right-hand rule
+            logger.info("**** Computing path");
+            try {
+                boolean pathFound = solver.solve();
+                if (pathFound) {
+                    logger.info("Path found!");
+                    // logger.info("Exit reached at: (" + maze.getGrid()[exitRow][exitCol] + ")");
+                    logger.info("Current position: (" + solver.getCurrentPosition().getRow() + ", " + solver.getCurrentPosition().getCol() + ")");
+                    logger.info("Direction faced: " + solver.getCurrentDirection());
+                }
+            } catch (Exception e) {
+                logger.error("Path not found: " + e.getMessage());
+            }
+
+        } catch (ParseException e) {
             logger.error("Failed to parse command-line arguments", e);
-        } catch(Exception e) {
+        } catch (Exception e) {
             logger.error("An error occurred while processing the maze file", e);
-        } 
-        logger.info("**** Computing path");
-        logger.info("PATH NOT COMPUTED");
+        }
+
         logger.info("** End of MazeRunner");
     }
 }
