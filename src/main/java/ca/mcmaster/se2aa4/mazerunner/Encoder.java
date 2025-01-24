0d153ea049d51389c12cc8e71eef31deebce40ba
tridibbanik17 <tridib.perfect@gmail.com>
2025-01-23 22:33:19 -0500

    Add updated version of encoder

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Encoder.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Encoder.java
@@ -1,5 +1,5 @@
-
 // import java.util.Scanner;
+package ca.mcmaster.se2aa4.mazerunner;
 
 public class Encoder {
     public static String encoder(String longText) {
@@ -8,13 +8,14 @@ public class Encoder {
         }
 
         StringBuilder encodedText = new StringBuilder("");
+        longText = longText.replaceAll("[\\s]", "");
         for (int i = 0; i < longText.length(); i++) {
             int count = 1;
             while ((i < longText.length() - 1) && (longText.charAt(i) == longText.charAt(i+1))) {
                 count++;
                 i++;
             }
-            encodedText.append(String.valueOf(longText.charAt(i)) + count);
+            encodedText.append(count + String.valueOf(longText.charAt(i)).toUpperCase() + " ");
         }
         return encodedText.toString();
     }
